e1a17a9553f5d9109025fd2ea3a28b97eee9b7bd
Juan Pablo <pablo@cruzf.net>
2012-01-31 04:05:19 +0100

    Event system

--- process.php
+++ process.php
@@ -1,12 +1,5 @@
 <?php session_start();
-		$user="root";
-		$host="localhost";
-		$password="";
-		$connection = mysql_connect($host,$user,$password)
-			or die ("couldn’t connect to server");
-		$database = "qes";
-		$db = mysql_select_db($database,$connection)
-			or die ("Couldn’t select database");
+include_once "connect.php";
 $id = $_SESSION['k_UserId'];
     function assertValidUpload($code)
     {
