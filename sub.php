5fa78721cf7cf36eac015d184a5e06caa2091a3c
juanpablocruz <pablo@cruzf.net>
2012-01-31 18:15:01 +0100

    Update sub.php

--- sub.php
+++ sub.php
@@ -20,14 +20,14 @@ $checkres = mysql_query($chck)
 	$query = "INSERT INTO member (loginName,password,createDate,email,phone)
 		VALUES ('$name','$pass','$hoy','$email','$movil')";
 	$result = mysql_query($query)
-		or die ("Couldn’t execute query.");
+		or die ("Couldn't execute query.");
 	$qry = ("SELECT * FROM member WHERE loginName = '$name'");
 	$res = mysql_query($qry)
 	or die(mysql_error());
 	$row= mysql_fetch_row($res);
 	$id = $row[5];
 	$table = "tabla".$id[0];
-	$query2 = mysql_query("CREATE TABLE $table(Sigo CHAR (20),Abierto CHAR (20))");
+	$query2 = mysql_query("CREATE TABLE $table(eventos int(20),Sigo CHAR (20),Abierto CHAR (20))");
   			echo
 			"<SCRIPT LANGUAGE='javascript'>
 			 location.href = 'index.html';
